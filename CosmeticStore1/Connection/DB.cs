70632ef740e8c0bbc8dd973ffae9107b5df4d279
ElzaM575 <mirzasalih2005@gmail.com>
2025-04-02 15:25:47 +0300

    последнее

--- CosmeticStore1/Connection/DB.cs
+++ CosmeticStore1/Connection/DB.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CosmeticStore1.Connection
 {
     internal class DB
     {
-        public static 
+        p
     }
 }
